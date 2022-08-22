bef6711607499430bfcd0a3dd973f81c97c0093b
Denis Fedash <0501054104d@gmail.com>
2022-08-22 21:30:03 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,19 @@
-// import { Route, Routes } from 'react-router-dom';
-// import { Toaster } from 'react-hot-toast';
-// import PageNotFound from 'pages/PageNotFound/PageNotFound';
+import { Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import PageNotFound from 'pages/PageNotFound/PageNotFound';
 
-// import DetailsPage from 'pages/DetailsPage/DetailsPage';
-// import HomePage from 'pages/HomePage/HomePages';
-
+import DetailsPage from 'pages/DetailsPage/DetailsPage';
 import HomePage from 'pages/HomePage/HomePages';
 
 export const App = () => {
   return (
     <>
-      <HomePage />
-
-      {/* <Routes>
-        <Route path="/*" element={<HomePage />} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="city/:id/" element={<DetailsPage />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-      <Toaster /> */}
+      <Toaster />
     </>
   );
 };
