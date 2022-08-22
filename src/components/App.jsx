d6ed5bee35e78dd1446a52254000e77aa7314366
Denis Fedash <0501054104d@gmail.com>
2022-08-22 20:23:31 +0300

    test 8

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import PageNotFound from 'pages/PageNotFound/PageNotFound';
+// import { Route, Routes } from 'react-router-dom';
+// import { Toaster } from 'react-hot-toast';
+// import PageNotFound from 'pages/PageNotFound/PageNotFound';
 
-import DetailsPage from 'pages/DetailsPage/DetailsPage';
-import HomePage from 'pages/HomePage/HomePages';
+// import DetailsPage from 'pages/DetailsPage/DetailsPage';
+// import HomePage from 'pages/HomePage/HomePages';
 import { Loader } from './Loader/Loader';
 import { Header } from './Header/Header';
-import { CardList } from './Cardlist/CardList';
+// import { CardList } from './Cardlist/CardList';
 
 export const App = () => {
   return (
     <>
       <Loader />
       <Header />
-      <CardList />
+      {/* <CardList /> */}
 
-      <Routes>
+      {/* <Routes>
         <Route path="/*" element={<HomePage />} />
         <Route path="city/:id/" element={<DetailsPage />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-      <Toaster />
+      <Toaster /> */}
     </>
   );
 };
