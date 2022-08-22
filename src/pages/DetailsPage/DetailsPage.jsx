9fd1cacb8b22948e54c86058168f20b8618b5950
Denis Fedash <0501054104d@gmail.com>
2022-08-22 18:49:18 +0300

    add libraries

---
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -0,0 +1,9 @@
+import { WeatherDetails } from 'components/Details/WeatherDetails';
+
+export const DetailsPage = () => {
+  return (
+    <>
+      <WeatherDetails />
+    </>
+  );
+};
---
+++ src/pages/HomePage/HomePages.jsx
@@ -0,0 +1,33 @@
+import { CardList } from 'components/Cardlist/CardList';
+import { Header } from 'components/Header/Header';
+import { Loader } from 'components/Loader/Loader';
+import { CITIES } from 'constants';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchWeather } from '../../redux/apiWeather';
+
+export const HomePage = () => {
+  const dispatch = useDispatch();
+  const { cityNames, cities, loading } = useSelector(({ weather }) => weather);
+
+  useEffect(() => {
+    const cities = localStorage.getItem(CITIES);
+    const parsedCities = cities ? JSON.parse(cities) : [];
+
+    Array.from(new Set(cityNames.concat(parsedCities))).forEach(cityName => {
+      dispatch(fetchWeather(cityName));
+    });
+  }, [dispatch, cityNames]);
+  return (
+    <>
+      {!loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <CardList cities={cities} />
+        </>
+      )}
+    </>
+  );
+};
